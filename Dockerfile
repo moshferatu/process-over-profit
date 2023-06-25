3eb16d118709e02bcdce61fa36c71a688b685ff6
moshferatu <gitea@steveharris.sh>
2023-06-25 12:26:42 -0700

    Add user mentions if they are in danger of being booted, markdown formatting for news events, and support for a .env file when developing locally

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.10
 ADD bot.py .
-RUN pip install discord.py requests
+RUN pip install discord.py python-dotenv requests
 CMD ["python", "./bot.py"]
--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import requests
 from datetime import datetime
 from discord import Intents
 from discord.ext.commands import Bot
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = Intents.all()
 intents.members = True
@@ -13,6 +16,8 @@ bot = Bot(intents=intents, command_prefix='!')
 @bot.command()
 async def test(context):
   await context.send('Test was successful!')
+
+do_not_boot_roles = ['DNB', 'MASTER DEBATERS', 'MODS', 'PROS']
 
 def batch_members(members, batch_size):
   for i in range(0, len(members), batch_size):
@@ -39,9 +44,17 @@ async def boot(context, message_link):
   bots = set(member for member in all_members if member.bot)
   members_to_boot = members_to_boot - bots
 
+  # Don't boot members belonging to privileged roles (e.g., MODS).
+  do_not_boot_members = set(member for member in members_to_boot 
+                         if any(role.name in do_not_boot_roles for role in member.roles))
+  members_to_boot = members_to_boot - do_not_boot_members
+
   sorted_members_to_boot = sorted(members_to_boot, key=lambda member: member.display_name.lower())
 
-  # TODO: Handle scenario where there are no members to boot.
+  if not sorted_members_to_boot:
+    await context.send('No members to :boot:')
+    return
+
   # Send multiple messages, if necessary, in order to avoid Discord API message length limit.
   for member_batch in batch_members(sorted_members_to_boot, 50):
     member_mentions = ', '.join([member.mention for member in member_batch])
@@ -50,7 +63,7 @@ async def boot(context, message_link):
 # Forex Factory news calendar.
 news_url = 'https://nfs.faireconomy.media/ff_calendar_thisweek.json'
 
-def parse_date(date_str):
+def get_date_time(date_str):
     return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S%z")
 
 @bot.command()
@@ -60,25 +73,27 @@ async def news(context):
   if news_response.status_code == 200:
       this_weeks_news_events = news_response.json()
       todays_news_events = [event for event in this_weeks_news_events
-                            if parse_date(event['date']).date() == now.date()
+                            if get_date_time(event['date']).date() == now.date()
                             and event['country'] == 'USD']
       if not todays_news_events:
          await context.send('No news for ' + now.strftime('%B %d'))
          return
       news_events_by_time = dict()
       for news_event in todays_news_events:
-          news_time = parse_date(news_event['date']).strftime('%I:%M %p ET')
+          news_date_time = get_date_time(news_event['date'])
+          news_time = news_date_time.strftime('%I:%M %p ET')
           news_title = news_event['title']
+          if news_date_time < datetime.now(news_date_time.tzinfo):
+            news_title = '~~' + news_title + '~~'
           if news_time not in news_events_by_time:
               news_events_by_time[news_time] = list()
           news_events_by_time[news_time].append(news_title)
-      # TODO: Only return news events that haven't occurred yet.
-      news_result = '# News for ' + now.strftime('%B %d')
+      news_response = '# News for ' + now.strftime('%B %d')
       for news_time in news_events_by_time.keys():
-          news_result += '\n## {time}'.format(time = news_time)
+          news_response += '\n## {time}'.format(time = news_time)
           for news_event in news_events_by_time[news_time]:
-              news_result += '\n* {event}'.format(event = news_event)
-      await context.send(news_result)
+              news_response += '\n* {event}'.format(event = news_event)
+      await context.send(news_response)
   else:
       await context.send('Error fetching news for ' + now.strftime('%B %d'))
 
